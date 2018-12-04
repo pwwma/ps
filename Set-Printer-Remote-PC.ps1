231c522957a2f2f90c91247f40a3deeb97e3d7fa
pwwma <peter.wahid@warnermusic.com>
2018-12-04 16:07:08 +1100

    Install a remote printer from a known Print Server

---
+++ Set-Printer-Remote-PC.ps1
@@ -0,0 +1,10 @@
+﻿<#
+    Remote install a printer, you must have local administrative rights on the remote PC
+#>
+
+Clear-Host
+
+$pcName = "PC name you have Admin rights to"
+$printerName ="\\PrintServer\Printer_Name"
+
+Invoke-Command -ComputerName $pcName -Scriptblock {RUNDLL32 PRINTUI.DLL,PrintUIEntry /ga /n$printerName }
